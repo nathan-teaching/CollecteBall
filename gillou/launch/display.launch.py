dd92ddb9519021edb18fda6e130b846ff246447b
hugo <hugo.reubrecht@ensta-bretagne.org>
2023-01-20 10:07:06 +0100

    aled

--- gillou/launch/display.launch.py
+++ gillou/launch/display.launch.py
@@ -2,9 +2,9 @@ import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
+from launch.actions import ExecuteProcess, RegisterEventHandler
 
-
-
+from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -21,38 +21,22 @@ def generate_launch_description():
     )
 
     world_path=os.path.join(pkg_share, 'world/my_world.sdf'),
+
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         parameters=[{'use_sim_time': use_sim_time}]
-        # condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+        
     )
-    #joint_state_publisher_gui_node = launch_ros.actions.Node(
-    #    package='joint_state_publisher_gui',
-    #    executable='joint_state_publisher_gui',
-    #    name='joint_state_publisher_gui'
-    #)
-    # rviz_node = launch_ros.actions.Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     name='rviz2',
-    #     output='screen',
-    #     arguments=['-d', LaunchConfiguration('rvizconfig')],
-    # )
 
-    #joint_state_publisher_gui_node = launch_ros.actions.Node(
-    #    package='joint_state_publisher_gui',
-    #    executable='joint_state_publisher_gui',
-    #    name='joint_state_publisher_gui',
-    #    condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    #)
     spawn_entity = launch_ros.actions.Node(
     package='gazebo_ros',
     executable='spawn_entity.py',
@@ -68,6 +52,20 @@ def generate_launch_description():
        parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
 )
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'effort_controllers'],
+        output='screen'
+    )
+
     
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
@@ -77,12 +75,24 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
         tennis_court_launch,
-        #joint_state_publisher_node=launch_ros_actions.Node(package='joint_state_publisher', executable='joint_state_publisher', name='joint_state_publisher', parameters=[{'use_sim_time':use_sim_time}]),
-        #joint_state_publisher_gui_node,
         robot_state_publisher_node,
         spawn_entity,
-        robot_localization_node
-        # rviz_node
+        robot_localization_node,
+        
+        load_joint_trajectory_controller,
+     
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        )
+      
     ])
-        # launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-        #                                     description='Absolute path to rviz config file'),
+        
